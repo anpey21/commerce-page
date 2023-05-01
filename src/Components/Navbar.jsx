a44eccc5d78c75e4ff0dc71f13dd6ec028d39af2
Anna Peysikova <anpey21@gmail.com>
2023-05-01 13:41:41 +0700

    fixed import issue

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { IoSearchOutline } from 'react-icons/io5';
 import { GiShoppingCart } from 'react-icons/gi';
